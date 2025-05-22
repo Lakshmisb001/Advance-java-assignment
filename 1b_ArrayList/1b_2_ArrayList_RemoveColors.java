8df791c111a89af2fd119c7d1cc8784a46be2028
Lakshmisb001 <llakshmisbanakar@gmail.com>
2025-05-22 22:47:07 +0530

    Update 1b_2_ArrayList_RemoveColors.java

--- 1b_ArrayList/1b_2_ArrayList_RemoveColors.java
+++ 1b_ArrayList/1b_2_ArrayList_RemoveColors.java
@@ -1,5 +1,5 @@
-/*Write a java program for getting different colors through ArrayList interface and delete nth
-element from the ArrayList object by using remove by index*/
+/*2. Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
 package ADV_JAVA;
 import java.util.ArrayList;
 public class ArrayList_RemoveColors {
